69bf4c58459aeb1ea72917a768113281c6858415
Sameer Singh <ssg8719@gmail.com>
2024-04-21 11:48:50 +0530

    done

--- lib/modules/gallery/bloc/gallery_event.dart
+++ lib/modules/gallery/bloc/gallery_event.dart
@@ -3,4 +3,5 @@ part of 'gallery_bloc.dart';
 @freezed
 class GalleryEvent with _$GalleryEvent {
   const factory GalleryEvent.started() = _Started;
+  const factory GalleryEvent.onTapBackButtonInFullScreenViewImageEvent() = _OnTapBackButtonInFullScreenViewImageEvent;
 }
