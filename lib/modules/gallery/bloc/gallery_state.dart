cb5cdb3d49120a5fb1f176ab9ae0de8b02867852
Sameer Singh <ssg8719@gmail.com>
2024-04-21 11:01:06 +0530

    done

--- lib/modules/gallery/bloc/gallery_state.dart
+++ lib/modules/gallery/bloc/gallery_state.dart
@@ -3,4 +3,6 @@ part of 'gallery_bloc.dart';
 @freezed
 class GalleryState with _$GalleryState {
   const factory GalleryState.initial() = _Initial;
+
+  const factory GalleryState.galleryImageLoadedState({required List<dynamic> images}) = _Initial;
 }
