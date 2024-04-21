8e2e168330bce0cc1c4cfe1ca6610df51b0492ff
Sameer Singh <ssg8719@gmail.com>
2024-04-21 10:58:07 +0530

    done

--- lib/modules/gallery/bloc/gallery_bloc.dart
+++ lib/modules/gallery/bloc/gallery_bloc.dart
@@ -1,14 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'gallery_bloc.freezed.dart';
 part 'gallery_event.dart';
 part 'gallery_state.dart';
-part 'gallery_bloc.freezed.dart';
 
 class GalleryBloc extends Bloc<GalleryEvent, GalleryState> {
+  List<dynamic> images = [];
+
   GalleryBloc() : super(const GalleryState.initial()) {
-    on<GalleryEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<GalleryEvent>((event, emit) {});
   }
 }
