7488c1759c652e2848b81185698cfa791c4add31
Sameer Singh <ssg8719@gmail.com>
2024-04-21 10:56:25 +0530

    done

--- lib/modules/gallery/ui/gallery_screen.dart
+++ lib/modules/gallery/ui/gallery_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_xplorer_test_task_sameer_singh/modules/gallery/bloc/gallery_bloc.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Gallery'),
-      ),
+    return BlocProvider(
+      create: (context) => GalleryBloc(),
+      child: Builder(builder: (context) {
+        final GalleryBloc bloc = context.read();
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Gallery'),
+          ),
+        );
+      }),
     );
   }
 }
